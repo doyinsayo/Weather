21b20a7f92e46009f14cb60d791580a99c8833a7
Doyinsayo Otufadebo <doyinsa01@gmail.com>
2020-08-20 12:28:23 +0100

    using the weather icons and implementing on the database

--- weathersite/weatherapp/models.py
+++ weathersite/weatherapp/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class City(models.Model):
+    name = models.CharField(max_length=50)
+    postcode = models.IntegerField()
+
+    def __str__(self):
+        #This is to allow the name of the city be shown in the database as it is typed
+        #and not as a Cityobject
+        return self.name   
+
+    class Meta:
+        #This is to pluralize the Model(City) in the database
+        verbose_name_plural = 'Cities'    
+        
--- weathersite/weatherapp/views.py
+++ weathersite/weatherapp/views.py
@@ -2,11 +2,22 @@ from django.shortcuts import render
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+from .models import City
 
 
 # Create your views here.
 def home(request):
-    url = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=a64b1b201212227009268a3a22578166'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=a64b1b201212227009268a3a22578166'
+    cities = City.objects.all() #return all the cities in the database
     city = 'Lagos'
     city_weather = requests.get(url.format(city)).json() #request the API data and convert the JSON to Python data types 
-    return render(request,'weatherapp/base.html')
+    weather = {
+        'city' : city,
+        'temperature' : city_weather['main']['temp'],
+        'description' : city_weather['weather'][0]['description'],
+        'icon' : city_weather['weather'][0]['icon']
+    }
+    stuff_for_frontend = {'weather' : weather}
+    return render(request,'weatherapp/base.html',stuff_for_frontend)
+
+    
